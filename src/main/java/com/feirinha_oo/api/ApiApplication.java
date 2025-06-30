9a9f00eeff199a8535d708671dc0e7c6c828fe83
Annylory LR <annyloryrosa@gmail.com>
2025-06-30 20:42:12 -0300

    initialized

---
+++ src/main/java/com/feirinha_oo/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.feirinha_oo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feirinha_oo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feirinha_oo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
